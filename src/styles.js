7c161e9098dae9f3c79062427493b85a3c48be68
Rian-usr <rian.jesus5@etec.sp.gov.br>
2023-08-07 22:02:25 -0300

    background

--- src/styles.js
+++ src/styles.js
@@ -3,8 +3,8 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;
   height: 100vh;
-  background: rgb(1,29,5);
-  background: linear-gradient(25deg, rgba(1,29,5,1) 9%, rgba(9,121,39,1) 62%, rgba(26,255,0,1) 100%);
+  background: rgb(31,82,39);
+  background: linear-gradient(137deg, rgba(31,82,39,1) 9%, rgba(9,121,39,1) 46%, rgba(95,255,77,1) 98%);
   display: flex;
   align-items: center;
   justify-content: center;
